5f146a6e3240892bbf0f9088fb3eb1a9a8fc2182
Jacques in office <258770530@qq.com>
2019-05-14 20:00:10 +0800

    获取相似APPID

--- update_app_similar.py
+++ update_app_similar.py
@@ -5,7 +5,7 @@
 @Contact :   258770530@qq.com.com
 @Modify Time      @Author        @Version    @Desciption
 ------------      -------        --------    -----------
-2019/5/13 14:37   Jacques Lim    1.0         None
+2019/5/13 14:37   Jacques Lim    1.0         输出update的sql文件，更新appinfo表里的similar字段
 '''
 import requests
 from db import Database
@@ -13,23 +13,31 @@ from bs4 import BeautifulSoup
 import re
 
 # APP链接文件路径
-app_links_path = './datafile/all_app_links_test.txt'
+app_links_path = './datafile/all_app_links_update.txt'
 # 更新数据库sql文件
-update_similar_sql_path = './datafile/update_similar.sql'
+update_similar_sql_path = './datafile/update_similar_{}.sql'
+# log文件
+update_app_similar_info_path = './log/update_app_similar_info.log'
 
 def update_app_similar(id):
     url = 'https://itunes.apple.com/cn/app/app/id{}'
-    html = requests.get(url.format(id), timeout=10)
-    soup = BeautifulSoup(html.text, 'html.parser')
-    str = soup.find(id='shoebox-ember-data-store').text
-    index = str.find('customersAlsoBoughtApps')
-    com = re.compile(r'"(\d+)"') #匹配“id”
-    similar = com.findall(str[index:index+700])
-    similar = [ int(x) for x in similar ] #字符串转数字
-    table = 'app_info'
-    field = "similar='{}'".format(similar)
-    condition = 'id={}'.format(id)
-    return Database.update_sql(table, field, condition)
+    try:
+        html = requests.get(url.format(id), timeout=10)
+        soup = BeautifulSoup(html.text, 'html.parser')
+        str = soup.find(id='shoebox-ember-data-store').text
+        index = str.find('customersAlsoBoughtApps')
+        com = re.compile(r'"(\d+)"') #匹配“id”
+        similar = com.findall(str[index:index+700])
+        similar = [ int(x) for x in similar ] #字符串转数字
+        table = 'app_info'
+        field = "similar='{}'".format(similar)
+        condition = 'id={}'.format(id)
+        return Database.update_sql(table, field, condition)
+    except:
+        print('id：{}：爬取失败'.format(id))
+        with open(update_app_similar_info_path, 'a+', encoding='utf8')as ff:
+            ff.write('id：{}：爬取失败'.format(id))
+        return None
 
 if __name__ == "__main__":
     with open(app_links_path, 'r', encoding='utf8')as file:
@@ -37,7 +45,7 @@ if __name__ == "__main__":
         for line in file:
             n += 1
             id = line.split('/')[6].replace('id', '').replace('?mt=8', '')
-            with open(update_similar_sql_path, 'a+', encoding='utf8')as ff:
+            with open(update_similar_sql_path.format(int(n/100000)), 'a+', encoding='utf8')as ff:
                 sql = update_app_similar(int(id))
                 if sql is not None:
                     ff.write(sql)
